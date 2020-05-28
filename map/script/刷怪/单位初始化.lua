8a8cc7002a65ade27ab2cf1ffa77760fcdeaf80c
huangjiale20 <47213997+huangjiale20@users.noreply.github.com>
2020-05-28 22:57:37 +0800

    Update 单位初始化.lua

--- map/script/刷怪/单位初始化.lua
+++ map/script/刷怪/单位初始化.lua
@@ -1,7 +1,7 @@
 
 local function get_difficult(degree,br)
     local base =1
-    local base_rate = br or 1.8
+    local base_rate = br or 1.65
     local degree = degree or 1 
     if degree == 1 then 
         return base 
