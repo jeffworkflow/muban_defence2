cd4308dd05f058101fd0858675ef20f202729b6a
huangjiale20 <47213997+huangjiale20@users.noreply.github.com>
2020-06-11 19:58:01 +0800

    Update 新人引导.lua

--- map/script/物品/杂类/新人引导.lua
+++ map/script/物品/杂类/新人引导.lua
@@ -6,17 +6,22 @@ mt{
     level = 1 ,
     wood = 20,
     --颜色
-    color = '紫',
+    -- color = '紫',
     tip = [[
+
 |cff00ff001、出来先完成任务【至凶之物】
+
 |cff00ffff2、然后F3传送练功房，在左上角的NPC处开始神魂修炼
+
 |cffffff003、在练功房右上角的NPC处，进行杀敌数抽奖
+
 |cffff00004、回基地左下角开始做【三眼灵猴】等主线任务（打不过就回练功房）
 
-|cffcccccc更多玩法自行摸索！愿君旗开得胜！|r]],
+|cffcccccc更多玩法自行摸索！愿君旗开得胜！|r
+]],
     --技能图标
     art = [[yxsm.blp]],
-    item_type ='消耗品'
+    -- item_type ='消耗品'
 }
 
 
