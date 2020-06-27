5d50dc51877e015cd3169d4eb25c9240d8ef2335
huangjiale20 <47213997+huangjiale20@users.noreply.github.com>
2020-06-27 22:09:57 +0800

    6-27-4

--- map/script/物品/商店/第六幕·魔神.lua
+++ map/script/物品/商店/第六幕·魔神.lua
@@ -91,7 +91,7 @@ function mt:on_cast_start()
                 ac.player.self:sendMsg('|cffffe799【系统消息】|cff00ffff '..p:get_name()..' |cff00ff00开启了“|cffff0000魔神的交易|cff00ff00”，在基地右下角npc-魔神之路中查看 ',5)
                 ac.player.self:sendMsg('|cffffe799【系统消息】|cff00ffff '..p:get_name()..' |cff00ff00开启了“|cffff0000魔神的交易|cff00ff00”，在基地右下角npc-魔神之路中查看 ',5)
             end
-            if p.cnt_total_succ == 2 and not ac.flag_mszs  then
+            if p.cnt_total_succ == 16 and not ac.flag_mszs  then
                 ac.flag_mszs = true
                 local unit = ac.find_unit('第六幕·魔神之路')
                 unit:add_sell_item('魔神之石',4)
--- map/script/英雄/hero_register.lua
+++ map/script/英雄/hero_register.lua
@@ -95,10 +95,10 @@ local function hero_register_main()
 		-- ac.wait(os.clock()*1000 % 1000 ,function()
 		-- end)
 		
-		player:create_pets(ac.peon_list[math.random(#ac.peon_list)])
+		-- player:create_pets(ac.peon_list[math.random(#ac.peon_list)])
 		
 		-- 创建宠物
-		-- player:create_pets('高级鸡')
+		player:create_pets('脸黑的羊')
 
 		-- player:selectUnit(player.peon)
 		
