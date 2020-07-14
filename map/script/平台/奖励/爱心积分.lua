97721693ee8aef77de8687ceb8722069e779e3f6
huangjiale20 <47213997+huangjiale20@users.noreply.github.com>
2020-07-14 12:17:38 +0800

    Update 爱心积分.lua

--- map/script/平台/奖励/爱心积分.lua
+++ map/script/平台/奖励/爱心积分.lua
@@ -2,24 +2,31 @@
 local mt = ac.skill['救救孩子']
 mt{
     level = 0,
-art = [[kaituozhe.blp]],
+art = [[jjhz.blp]],
 tip = [[|cffffff00【要求地图等级>%need_map_level%|cffffff00】|r
 
 |cffffe799【获得方式】：|r
-|cff00ff00贪婪魔窟|cffff0000最高|cff00ff00层数大于|cffffff00 25层 |cff00ff00获得
+|cff00ffff爱心积分≥1000%current%
 
-|cffffe799【成就属性】|r:|cff00ff00
-|cff00ff00+100  杀怪加全属性|r
-|cff00ff00+50   攻击减甲|r
-|cff00ff00+50% 暴击伤害|r
-|cff00ff00+25% 物理伤害加深|r
+|cffFFE799【成就属性】：|r
+|cff00ff00+50  杀怪加全属性|r
+|cff00ff00+25  攻击减甲|r
+|cff00ff00+5   每秒加护甲|r
+|cff00ff00+10% 对BOSS额外伤害|r
 
 ]],
 need_map_level = 10,
-['杀怪加全属性'] = 100,
-['攻击减甲'] = 50,
-['暴击伤害'] = 50,
-['物理伤害加深'] = 25,
+current = function(self)
+    local p = ac.player.self
+    local val = p.server and p.server['爱心积分'] or 0
+    local str = '|cffdf19d0（当前爱心积分：|cffffe799'..val..'|cffdf19d0）'
+    return str
+end,
+
+['杀怪加全属性'] = 50,
+['攻击减甲'] = 25,
+['每秒加护甲'] = 5,
+['对BOSS额外伤害'] = 10,
 
 }
 
@@ -28,24 +35,155 @@ need_map_level = 10,
 local mt = ac.skill['关爱萌新']
 mt{
     level = 0,
-art = [[maoxianjia.blp]],
+art = [[mengxin.blp]],
 tip = [[|cffffff00【要求地图等级>%need_map_level%|cffffff00】|r
 
 |cffffe799【获得方式】：|r
-|cff00ff00贪婪魔窟|cffff0000最高|cff00ff00层数大于|cffffff00 50层 |cff00ff00获得
+|cff00ffff爱心积分≥2500%current%
 
-|cffffe799【成就属性】|r:|cff00ff00
-|cff00ff00+150  杀怪加全属性|r
-|cff00ff00+75   攻击减甲|r
-|cff00ff00+50% 技暴伤害|r
-|cff00ff00+25% 技能伤害加深|r
+|cffFFE799【成就属性】：|r
+|cff00ff00+100  杀怪加全属性|r
+|cff00ff00+50  攻击减甲|r
+|cff00ff00+10   每秒加护甲|r
+|cff00ff00+20% 对BOSS额外伤害|r
 
 ]],
-need_map_level = 15,
+need_map_level = 12,
+current = function(self)
+    local p = ac.player.self
+    local val = p.server and p.server['爱心积分'] or 0
+    local str = '|cffdf19d0（当前爱心积分：|cffffe799'..val..'|cffdf19d0）'
+    return str
+end,
+
+['杀怪加全属性'] = 100,
+['攻击减甲'] = 50,
+['每秒加护甲'] = 10,
+['对BOSS额外伤害'] = 20,
+
+}
+
+local mt = ac.skill['新人辅导员']
+mt{
+    level = 0,
+art = [[xrfdy.blp]],
+tip = [[|cffffff00【要求地图等级>%need_map_level%|cffffff00】|r
+
+|cffffe799【获得方式】：|r
+|cff00ffff爱心积分≥5000%current%
+
+|cffFFE799【成就属性】：|r
+|cff00ff00+150  杀怪加全属性|r
+|cff00ff00+75  攻击减甲|r
+|cff00ff00+15   每秒加护甲|r
+|cff00ff00+30% 对BOSS额外伤害|r
+
+]],
+need_map_level = 14,
+current = function(self)
+    local p = ac.player.self
+    local val = p.server and p.server['爱心积分'] or 0
+    local str = '|cffdf19d0（当前爱心积分：|cffffe799'..val..'|cffdf19d0）'
+    return str
+end,
+
 ['杀怪加全属性'] = 150,
 ['攻击减甲'] = 75,
-['技暴伤害'] = 50,
-['技能伤害加深'] = 25,
+['每秒加护甲'] = 15,
+['对BOSS额外伤害'] = 30,
+
+}
+
+local mt = ac.skill['无私奉献']
+mt{
+    level = 0,
+art = [[wsfx.blp]],
+tip = [[|cffffff00【要求地图等级>%need_map_level%|cffffff00】|r
+
+|cffffe799【获得方式】：|r
+|cff00ffff爱心积分≥10000%current%
+
+|cffFFE799【成就属性】：|r
+|cff00ff00+200  杀怪加全属性|r
+|cff00ff00+100  攻击减甲|r
+|cff00ff00+20   每秒加护甲|r
+|cff00ff00+40% 对BOSS额外伤害|r
+
+]],
+need_map_level = 16,
+current = function(self)
+    local p = ac.player.self
+    local val = p.server and p.server['爱心积分'] or 0
+    local str = '|cffdf19d0（当前爱心积分：|cffffe799'..val..'|cffdf19d0）'
+    return str
+end,
+
+['杀怪加全属性'] = 200,
+['攻击减甲'] = 100,
+['每秒加护甲'] = 20,
+['对BOSS额外伤害'] = 40,
+
+}
+
+local mt = ac.skill['爱心之星']
+mt{
+    level = 0,
+art = [[axzx.blp]],
+tip = [[|cffffff00【要求地图等级>%need_map_level%|cffffff00】|r
+
+|cffffe799【获得方式】：|r
+|cff00ffff爱心积分≥20000%current%
+
+|cffFFE799【成就属性】：|r
+|cff00ff00+250  杀怪加全属性|r
+|cff00ff00+125  攻击减甲|r
+|cff00ff00+25   每秒加护甲|r
+|cff00ff00+50% 对BOSS额外伤害|r
+
+]],
+need_map_level = 18,
+current = function(self)
+    local p = ac.player.self
+    local val = p.server and p.server['爱心积分'] or 0
+    local str = '|cffdf19d0（当前爱心积分：|cffffe799'..val..'|cffdf19d0）'
+    return str
+end,
+
+['杀怪加全属性'] = 250,
+['攻击减甲'] = 125,
+['每秒加护甲'] = 25,
+['对BOSS额外伤害'] = 50,
+
+}
+
+local mt = ac.skill['爱心大使']
+mt{
+    level = 0,
+art = [[axds.blp]],
+tip = [[|cffffff00【要求地图等级>%need_map_level%|cffffff00】|r
+
+|cffffe799【获得方式】：|r
+|cff00ffff爱心积分≥30000%current%
+
+|cffFFE799【成就属性】：|r
+|cff00ff00+300  杀怪加全属性|r
+|cff00ff00+150  攻击减甲|r
+|cff00ff00+30   每秒加护甲|r
+|cff00ff00+60% 对BOSS额外伤害|r
+
+]],
+need_map_level = 20,
+current = function(self)
+    local p = ac.player.self
+    local val = p.server and p.server['爱心积分'] or 0
+    local str = '|cffdf19d0（当前爱心积分：|cffffe799'..val..'|cffdf19d0）'
+    return str
+end,
+
+['杀怪加全属性'] = 300,
+['攻击减甲'] = 150,
+['每秒加护甲'] = 30,
+['对BOSS额外伤害'] = 60,
 
 }
 
@@ -54,16 +192,22 @@ local mt = ac.skill['关爱萌新成就']
 mt{
     is_spellbook = 1,
     is_order = 2,
-    title = '关爱萌新成就',
-    art = [[dashoucangjia.blp]],
+    title = '关爱萌新',
+    art = [[mengxin.blp]],
     tip = [[
 
-点击查看 |cff00ffff关爱萌新就|r，主要通过 |cffffff00不断挑战魔窟|r 获得
-    ]],
+点击查看 |cff00ffff关爱萌新成就|r，主要通过 |cffffff00不断带萌新通关|r 获得
+
+|cffebb608【说明】|r
+|cff00ff00老司机|cffffff00（地图等级≥10的玩家）|cff00ff00带萌新|cffffff00（地图等级≤5的玩家）|cff00ff00通关，能获得可存档的爱心积分
+
+|cffff0000当爱心积分达到一定值，能自动解锁可存档成就
+    
+|cffcccccc（萌新数量越多、游戏难度越高，获得的爱心积分越多）]],
     
 }
 mt.skills = {
-    '救救孩子','关爱萌新',
+    '救救孩子','关爱萌新','新人辅导员','无私奉献','爱心之星','爱心大使',
 }
 local function get_player_cnt_bylv(lv)
     local lv = lv or 1 
@@ -106,7 +250,7 @@ ac.game:event '游戏-结束' (function(trg,flag)
                 p:AddServerValue('today_'..key,val) 
                 --累计总榜
                 p:AddServerValue(key,val) 
-                p:sendMsg('恭喜获得关爱萌新积分: '..val)
+                p:sendMsg('|cffebb608【系统】|r|cff00ff00本次通关获得爱心积分: |cffff0000'..val)
                 print('恭喜获得关爱萌新积分: '..val)
             end
         end
