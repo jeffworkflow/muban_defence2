f194a884a45d40dc4da62228d3ad7f1c46b57651
DESKTOP-1LO98I2\Mloong <418796833@qq.com>
2020-08-24 22:25:21 +0800

    Update 噬血珠.lua

--- map/script/物品/装备/噬血珠.lua
+++ map/script/物品/装备/噬血珠.lua
@@ -192,6 +192,9 @@ function mt:on_cast_start()
                 end    
                 --传送回练功房
                 local point = ac.map.rects['练功房刷怪'..p.id]:get_point()
+                if finds(ac.g_game_degree_name,'深渊冒险') then 
+                    point = ac.rect.j_rect('zxzw1'):get_point()
+                end
                 hero:blink(point,true,false,true)
             end)
         end)
