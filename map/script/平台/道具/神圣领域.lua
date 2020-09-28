4cae3825e6c9569d8a5c12ec14c771f191f29138
DESKTOP-1LO98I2\Mloong <418796833@qq.com>
2020-09-29 00:00:07 +0800

    3.93

--- map/script/平台/道具/神圣领域.lua
+++ map/script/平台/道具/神圣领域.lua
@@ -696,7 +696,42 @@ need_map_level = 42,
 effect = [[2tx_91.mdx]]
 }
 
-for i,name in ipairs({'血雾领域','龙腾领域','迦蓝领域','极寒领域','黄龙天翔领域','焚魂镇妖领域','真武青焰领域','琉光十色领域','孤风青龙领域','远影苍龙领域'}) do
+local mt = ac.skill['血色领域']
+mt{
+--等级
+level = 0,
+is_order = 1,
+--图标
+art = [[chenghao1.blp]],
+--说明
+tip = [[
+|cffffff00【要求地图等级>%need_map_level%|cffffff00】|r
+
+|cffffe799【获得方式】：|r
+|cff00ffff种树熟练度≥1000%current%
+
+|cffFFE799【称号属性】：|r
+|cff00ff00+18  杀怪加全属性|r
+|cff00ff00+1   每秒加木头|r
+|cff00ff00+5% 全伤加深|r
+
+|cffff0000【点击可更换称号外观，所有称号属性可叠加】|r]],
+--目标类型
+target_type = ac.skill.TARGET_TYPE_NONE,
+['杀怪加全属性'] = 18,
+['全伤加深'] = 5,
+['每秒加木头'] = 1,
+['全伤加深系数'] = function(self)
+    if not self.owner then return 0 end
+    local p =  self.owner.owner
+    return p:Map_GetMapLevel()*1
+end,
+need_map_level = 1,
+--特效
+effect = [[linghunguanghuan.mdx]]
+}
+
+for i,name in ipairs({'血色领域','血雾领域','龙腾领域','迦蓝领域','极寒领域','黄龙天翔领域','焚魂镇妖领域','真武青焰领域','琉光十色领域','孤风青龙领域','远影苍龙领域'}) do
     local mt = ac.skill[name]
     function mt:on_cast_start()
         local hero = self.owner
