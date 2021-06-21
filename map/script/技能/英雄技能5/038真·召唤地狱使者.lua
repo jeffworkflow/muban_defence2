79712b158d0b96150b075c23d366013257d94cfa
huangjiale20 <47213997+huangjiale20@users.noreply.github.com>
2021-06-21 19:18:27 +0800

    Update 038真·召唤地狱使者.lua

--- map/script/技能/英雄技能5/038真·召唤地狱使者.lua
+++ map/script/技能/英雄技能5/038真·召唤地狱使者.lua
@@ -81,7 +81,7 @@ function mt:create_illusion(damage)
         attribute = attribute,
         remove_target = true,
         skill = self,
-        follow = true,
+        -- follow = true,
         search_area = 1000, 
     }
 
@@ -91,7 +91,7 @@ function mt:create_illusion(damage)
 		source = u,
 		skill = self,
 		pulse = 1,
-		-- keep = true,
+		keep = true,
 		is_damage_on_add = false,
 		damage = attribute['攻击']*10*self.level,
 	}
